56a1a33dd75028aef1baf4bde453f3def522a6cf
Alec <alec@deviant-logic.net>
2015-09-18 16:30:00 -0400

    don't check servant stuff either

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -4,6 +4,9 @@
 
   haskellPackageOverrides = with pkgs.haskell.lib; self: super: {
     lens = dontCheck super.lens;
+    servant = dontCheck super.servant;
+    servant-client = dontCheck super.servant-client;
+    servant-server = dontCheck super.servant-server;
     mkDerivation = expr: super.mkDerivation (expr // { enableLibraryProfiling = true; });
   };
 }
